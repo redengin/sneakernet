966e35319c7cf67ceae51baf61833ed31ab299c2
redengin <sholstei@yahoo.com>
2023-06-07 02:58:44 -0400

    wifi-scan working

--- flutter/sneakernet/lib/main.dart
+++ flutter/sneakernet/lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:location_permissions/location_permissions.dart';
+import 'package:wifi_scan/wifi_scan.dart';
+import 'package:workmanager/workmanager.dart';
 import 'pages/root.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  // setup background wifi scan for sneakernet nodes
+  PermissionStatus permission = await LocationPermissions().requestPermissions(permissionLevel: LocationPermissionLevel.locationAlways);
+  StreamSubscription<List<WiFiAccessPoint>> subscription = WiFiScan.instance.onScannedResultsAvailable.listen((results) => _handleScanResults(results));
+  Workmanager().initialize(
+      callbackDispatcher, // The top level function, aka callbackDispatcher
+      isInDebugMode: true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+  );
+  int taskId = 0;
+  Workmanager().registerPeriodicTask((++taskId).toString(), scanTask_name,
+    backoffPolicy: BackoffPolicy.exponential, backoffPolicyDelay: Duration(seconds: 10));
+
   runApp(const MyApp());
 }
 
@@ -21,3 +39,31 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+/*------------------------------------------------------------------------------
+Background Tasks
+------------------------------------------------------------------------------*/
+const scanTask_name = "sneakernet-wifi-scan";
+@pragma('vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+void callbackDispatcher() {
+  Workmanager().executeTask((task_name, inputData) async {
+    switch(task_name) {
+      case scanTask_name:
+        final can = await WiFiScan.instance.canStartScan(askPermissions: true);
+        switch(can) {
+          case CanStartScan.yes:
+            return WiFiScan.instance.startScan();
+          default:
+            return Future.error("unable to scan wifi");
+        }
+      default:
+        return Future.error("unknown task");
+    }
+  });
+}
+
+const SNEAKER_NET_SSID = "SneakerNet";
+_handleScanResults(results) {
+  results.removeWhere((wifiNode) => ! wifiNode.ssid.startsWith(SNEAKER_NET_SSID));
+}
+
