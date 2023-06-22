ad607ce140bbe6d3215c2e4276018cd5a94a02e8
redengin <sholstei@yahoo.com>
2023-06-22 01:21:49 -0400

    background sync task working (need to implement sync)

--- flutter/sneakernet/lib/sneakernet.dart
+++ flutter/sneakernet/lib/sneakernet.dart
@@ -6,8 +6,13 @@ SplayTreeSet<WiFiAccessPoint> localSneakerNets =
     SplayTreeSet<WiFiAccessPoint>((a, b) => a.bssid.compareTo(b.bssid));
 
 class SneakerNet {
-    static void sync(WiFiAccessPoint sneakerNet) {
-        flutterLocalNotificationsPlugin.show(
-            SNEAKERNET_SYNC_ID, 'SneakerNets syncing...', sneakerNet.ssid, notificationDetails);
-    }
+  static void sync(WiFiAccessPoint sneakerNet) async {
+    flutterLocalNotificationsPlugin.show(SNEAKERNET_SYNC_ID,
+        'SneakerNets syncing...', sneakerNet.ssid, notificationDetails);
+    // if (!await WifiConnector.connectToWifi(ssid: sneakerNet.ssid)) {
+    //   flutterLocalNotificationsPlugin.show(SNEAKERNET_SYNC_ID, 'SneakerNets',
+    //       "Unable to connect to $sneakerNet.ssid", notificationDetails);
+    //   return;
+    // }
+  }
 }
