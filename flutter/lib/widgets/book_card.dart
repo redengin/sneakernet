af2e5ad917446853b0c762d3a1593880f36d658f
redengin <sholstei@yahoo.com>
2023-07-03 02:06:43 -0400

    import working-ish - epubx is shit TODO make a real epub library

--- flutter/lib/widgets/book_card.dart
+++ flutter/lib/widgets/book_card.dart
@@ -19,22 +19,23 @@ class _BookCardState extends State<BookCard> {
           ListTile(
             leading: FutureBuilder<Epub.EpubBookRef>(
               future: widget.book,
-              builder: (context, snapshot) => snapshot.hasData
+              builder: (context, snapshot) =>
+              snapshot.hasData
                   ? _Cover(snapshot.data!.readCover())
                   : const Icon(Icons.note),
             ),
             title: FutureBuilder<Epub.EpubBookRef>(
                 future: widget.book,
                 builder: (context, snapshot) =>
-                    (snapshot.hasData && (snapshot.data!.Title != null))
-                        ? Text(snapshot.data!.Title!)
-                        : const Text("")),
+                (snapshot.hasData && (snapshot.data!.Title != null))
+                    ? Text(snapshot.data!.Title!)
+                    : const Text("")),
             subtitle: FutureBuilder<Epub.EpubBookRef>(
                 future: widget.book,
                 builder: (context, snapshot) =>
-                    (snapshot.hasData && (snapshot.data!.Author != null))
-                        ? Text(snapshot.data!.Author!)
-                        : const Text("")),
+                (snapshot.hasData && (snapshot.data!.Author != null))
+                    ? Text(snapshot.data!.Author!)
+                    : const Text("")),
           ),
           // actions
           const Row(
@@ -45,17 +46,11 @@ class _BookCardState extends State<BookCard> {
       ),
     );
   }
-
-// Widget _icon_builder(BuildContext context, AsyncSnapshot<Epub.EpubBookRef> snapshot) {
-//   if(snapshot.hasData && (snapshot.data!.CoverImage != null)) {
-//     return Image.memory(snapshot.data!.CoverImage!.getBytes());
-//   }
-//   return Icon(Icons.note);
-// }
 }
 
 class _Cover extends StatefulWidget {
   final Future<Epub.Image?> cover;
+
   _Cover(this.cover);
 
   @override
@@ -67,39 +62,10 @@ class _CoverState extends State<_Cover> {
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: widget.cover,
-      builder: (context, snapshot) => snapshot.hasData
+      builder: (context, snapshot) =>
+      snapshot.hasData
           ? Image.memory(snapshot.data!.getBytes())
           : const Icon(Icons.downloading),
     );
   }
 }
-
-// Widget build(BuildContext context) {
-//   return Card(
-//     child: Column(
-//       children: [
-//     ListTile(
-//     leading:
-//     const SizedBox(height: 100.0, width: 100.0,
-//       child: FutureBuilder<EpubBook>(
-//           future: book,
-//           builder: _icon_builder),
-//       // Icon(Icons.note)),
-//       // ebook.CoverImage.isDefinedAndNotNull ?
-//       // Image.memory(ebook.CoverImage.getBytes())
-//       //     : Icon(Icons.note)),
-//       // title: Text(ebookRef.Title ?? ""),
-//     ),
-//     // actions
-//     const Row(
-//       mainAxisAlignment: MainAxisAlignment.end,
-//       children: [],
-//     )
-//     ],
-//   ));
-// }
-//
-// Widget _icon_builder(BuildContext context, AsyncSnapshot<EpubBook> snapshot) {
-//   return const Text("nothing");
-// }
-//
