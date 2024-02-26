d4fe8265e0f9b4f3a5ca47eca7f5dc96f507e08f
redengin <sholstei@yahoo.com>
2024-02-25 22:47:38 -0500

    refactoring

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
+
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:wifi_scan/wifi_scan.dart';
+
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:wifi_scan/wifi_scan.dart';
+
 import 'package:workmanager/workmanager.dart';
 import 'package:path/path.dart' as p;
-import 'dart:async';
-import 'dart:io';
 
 import 'library.dart';
 import 'settings.dart';
 import 'notifications.dart';
 import 'sneakernet.dart';
+
 // import 'pages/settings.dart';
 import 'pages/about.dart';
 import 'pages/library.dart';
@@ -30,51 +32,50 @@ Future<void> main() async {
   final libraryPath = await getTemporaryDirectory();
   library = Library(libraryPath);
 
+  // enable notifications
+  await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()?.requestNotificationsPermission();
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
+  // FIXME - display a notification
+  await flutterLocalNotificationsPlugin.show(
+      0, 'plain title', 'plain body', notificationDetails,
+      payload: 'item x');
+  await flutterLocalNotificationsPlugin.show(
+      0, 'plain title', 'plain body 1', notificationDetails,
+      payload: 'item x 1');
+
   // create background tasks
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   Workmanager().initialize(callbackDispatcher);
+
   // subscribe to wifi scans
   final scanSubscription =
-  WiFiScan.instance.onScannedResultsAvailable.listen((results) {
+      WiFiScan.instance.onScannedResultsAvailable.listen((results) {
     var sneakerNetNodes = results
         .where((_) => _.ssid.startsWith(sneakerNetPrefix))
         .toList(growable: false);
 
     if (sneakerNetNodes.isNotEmpty) {
-      var ssids = sneakerNetNodes.map((_) => _.ssid).toList();
-
-      // if (settings.getDoNotify()) {
-        flutterLocalNotificationsPlugin.show(SNEAKERNETS_FOUND_ID,
-            'SneakerNets found', ssids.join(','), notificationDetails);
-      // }
+      // var ssids = sneakerNetNodes.map((_) => _.ssid).toList();
 
       // if (settings.getAutoSync()) {
-        ssids.forEach((ssid) {
-          // TODO only supports android
-          Workmanager().registerOneOffTask(syncTaskName, ssid,
-            existingWorkPolicy:ExistingWorkPolicy.append,
-            inputData: {
-              syncTaskParamSsid: ssid,
-            });
-        });
-        // Workmanager().registerOneOffTask(
-        //     SNEAKERNET_SYNC_ID.toString(), syncTaskName,
-        //     inputData: {
-        //       syncTaskParamSneakernets: ssids,
-        //       syncTaskParamLibraryPath: libraryDir.path,
-        //     });
+      //   ssids.forEach((ssid) {
+      //     Workmanager().registerOneOffTask(syncTaskName, ssid,
+      //         existingWorkPolicy: ExistingWorkPolicy.append,
+      //         inputData: {
+      //           syncTaskParamSsid: ssid,
+      //           syncTaskParamLibraryPath: libraryPath,
+      //         });
+      //   });
       // }
     }
   });
-  // start the wifi scan background task
-  Workmanager().registerPeriodicTask(scanTaskName, scanTaskName);
-
 
   // decide which page to start based upon how we were launched
-  final NotificationAppLaunchDetails? notificationAppLaunchDetails = !kIsWeb &&
-          Platform.isLinux
-      ? null
-      : await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+  // final NotificationAppLaunchDetails? notificationAppLaunchDetails = !kIsWeb &&
+  //         Platform.isLinux
+  //     ? null
+  //     : await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
 
   var initialRoute = LibraryPage.routeName;
 
@@ -104,10 +105,13 @@ Future<void> main() async {
   ));
 }
 
+
+void onDidReceiveNotificationResponse(NotificationResponse details) {
+}
+
 /*------------------------------------------------------------------------------
 WorkManager Helpers
 ------------------------------------------------------------------------------*/
-const scanTaskName = 'wifi-scan';
 const syncTaskName = 'sneakernet-sync';
 const syncTaskParamSsid = 'sneakernet-ssid';
 const syncTaskParamLibraryPath = 'libraryPath';
@@ -117,14 +121,6 @@ const syncTaskParamLibraryPath = 'libraryPath';
 void callbackDispatcher() {
   Workmanager().executeTask((taskName, inputData) async {
     switch (taskName) {
-      case scanTaskName:
-        switch (await WiFiScan.instance.canStartScan(askPermissions: true)) {
-          case CanStartScan.yes:
-            return WiFiScan.instance.startScan();
-          default:
-            return false;
-        }
-
       case syncTaskName:
         // validate invocation
         final ssid = inputData?[syncTaskParamSsid];
@@ -143,3 +139,4 @@ void callbackDispatcher() {
     }
   });
 }
+
--- flutter/lib/notifications.dart
+++ flutter/lib/notifications.dart
@@ -1,165 +1,21 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-// unique notification ids
-const SNEAKERNETS_FOUND_ID = 1;
-const SNEAKERNET_SYNC_ID = 2;
-const SNEAKERNET_WIFI_ERROR_ID = 3;
+final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@drawable/ic_launcher_foreground');
+final InitializationSettings initializationSettings =
+    InitializationSettings(android: initializationSettingsAndroid);
 
-// default notification details
-const NotificationDetails notificationDetails = NotificationDetails(
-  android: _androidNotificationDetails,
-  iOS: _iosNotificationDetails,
-  macOS: _macOSNotificationDetails,
-  linux: _linuxNotificationDetails,
-);
+const channelId = 'sneakernet';
+const channelName = 'sneakernet';
+const AndroidNotificationDetails androidNotificationDetails =
+    AndroidNotificationDetails(channelId, channelName,
+        channelDescription: 'sneakernet notifications',
+        importance: Importance.max,
+        priority: Priority.high,
+        ticker: 'ticker');
+const NotificationDetails notificationDetails =
+    NotificationDetails(android: androidNotificationDetails);
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-FlutterLocalNotificationsPlugin();
 
-final InitializationSettings initializationSettings = InitializationSettings(
-  android: _initializationSettingsAndroid,
-  linux: _initializationSettingsLinux,
-  iOS: _initializationSettingsDarwin,
-  macOS: _initializationSettingsDarwin,
-);
-
-const AndroidInitializationSettings _initializationSettingsAndroid =
-    AndroidInitializationSettings('@mipmap/launcher_icon');
-
-final LinuxInitializationSettings _initializationSettingsLinux =
-    LinuxInitializationSettings(
-  defaultActionName: 'Open notification',
-  defaultIcon: AssetsLinuxIcon('icon.png'),
-);
-
-/// A notification action which triggers a App navigation event
-const String navigationActionId = 'id_3';
-
-/// Defines a iOS/MacOS notification category for text input actions.
-const String darwinNotificationCategoryText = 'textCategory';
-
-/// Defines a iOS/MacOS notification category for plain actions.
-const String darwinNotificationCategoryPlain = 'plainCategory';
-final List<DarwinNotificationCategory> _darwinNotificationCategories =
-    <DarwinNotificationCategory>[
-  DarwinNotificationCategory(
-    darwinNotificationCategoryText,
-    actions: <DarwinNotificationAction>[
-      DarwinNotificationAction.text(
-        'text_1',
-        'Action 1',
-        buttonTitle: 'Send',
-        placeholder: 'Placeholder',
-      ),
-    ],
-  ),
-  DarwinNotificationCategory(
-    darwinNotificationCategoryPlain,
-    actions: <DarwinNotificationAction>[
-      DarwinNotificationAction.plain('id_1', 'Action 1'),
-      DarwinNotificationAction.plain(
-        'id_2',
-        'Action 2 (destructive)',
-        options: <DarwinNotificationActionOption>{
-          DarwinNotificationActionOption.destructive,
-        },
-      ),
-      DarwinNotificationAction.plain(
-        navigationActionId,
-        'Action 3 (foreground)',
-        options: <DarwinNotificationActionOption>{
-          DarwinNotificationActionOption.foreground,
-        },
-      ),
-      DarwinNotificationAction.plain(
-        'id_4',
-        'Action 4 (auth required)',
-        options: <DarwinNotificationActionOption>{
-          DarwinNotificationActionOption.authenticationRequired,
-        },
-      ),
-    ],
-    options: <DarwinNotificationCategoryOption>{
-      DarwinNotificationCategoryOption.hiddenPreviewShowTitle,
-    },
-  )
-];
-
-final DarwinInitializationSettings _initializationSettingsDarwin =
-    DarwinInitializationSettings(
-  requestAlertPermission: false,
-  requestBadgePermission: false,
-  requestSoundPermission: false,
-  // FIXME
-  // onDidReceiveLocalNotification:
-  //     (int id, String? title, String? body, String? payload) async {
-  //   didReceiveLocalNotificationStream.add(
-  //     ReceivedNotification(
-  //       id: id,
-  //       title: title,
-  //       body: body,
-  //       payload: payload,
-  //     ),
-  //   );
-  // },
-  notificationCategories: _darwinNotificationCategories,
-);
-
-const AndroidNotificationDetails _androidNotificationDetails =
-    AndroidNotificationDetails(
-  'your channel id',
-  'your channel name',
-  channelDescription: 'your channel description',
-  importance: Importance.max,
-  priority: Priority.high,
-  ticker: 'ticker',
-  // FIXME provide actions
-  // actions: <AndroidNotificationAction>[
-  //   AndroidNotificationAction(
-  //     urlLaunchActionId,
-  //     'Action 1',
-  //     icon: DrawableResourceAndroidBitmap('food'),
-  //     contextual: true,
-  //   ),
-  //   AndroidNotificationAction(
-  //     'id_2',
-  //     'Action 2',
-  //     titleColor: Color.fromARGB(255, 255, 0, 0),
-  //     icon: DrawableResourceAndroidBitmap('secondary_icon'),
-  //   ),
-  //   AndroidNotificationAction(
-  //     navigationActionId,
-  //     'Action 3',
-  //     icon: DrawableResourceAndroidBitmap('secondary_icon'),
-  //     showsUserInterface: true,
-  //     // By default, Android plugin will dismiss the notification when the
-  //     // user tapped on a action (this mimics the behavior on iOS).
-  //     cancelNotification: false,
-  //   ),
-  // ],
-);
-
-const LinuxNotificationDetails _linuxNotificationDetails =
-LinuxNotificationDetails(
-  actions: <LinuxNotificationAction>[
-    // FIXME provide actions
-    // LinuxNotificationAction(
-    //   key: urlLaunchActionId,
-    //   label: 'Action 1',
-    // ),
-    // LinuxNotificationAction(
-    //   key: navigationActionId,
-    //   label: 'Action 2',
-    // ),
-  ],
-);
-
-const DarwinNotificationDetails _iosNotificationDetails =
-DarwinNotificationDetails(
-  categoryIdentifier: darwinNotificationCategoryPlain,
-);
-
-const DarwinNotificationDetails _macOSNotificationDetails =
-DarwinNotificationDetails(
-  categoryIdentifier: darwinNotificationCategoryPlain,
-);
+const SNEAKERNET_SYNC_ID = 100;
