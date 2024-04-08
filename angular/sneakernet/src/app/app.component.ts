96b38266d923f692e278922e1a15f2e24f2cff6a
redengin <sholstei@yahoo.com>
2024-04-07 23:39:38 -0400

    angular app first attempt - to be tested

--- angular/sneakernet/src/app/app.component.ts
+++ angular/sneakernet/src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list'; 
+import {NgFor} from '@angular/common'; 
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {Observable, lastValueFrom } from 'rxjs';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+      CommonModule,
+      RouterOutlet,
+      MatToolbarModule,
+      MatIconModule,
+      MatListModule,
+      HttpClientModule,
+      NgFor,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sneakernet';
+  title = 'SneakerNet';
+
+  constructor(private http: HttpClient) {}
+
+  getCatalog() : Observable<SneakerNetFile[]> {
+    return this.http.get<SneakerNetFile[]>('/catalog', {responseType:'json'});
+  }
+
+  delete(filename: string)
+  {
+    this.http.delete('/catalog/' + filename);
+  }
+}
+export interface SneakerNetFile {
+  filename: string,
+  timestamp: string,
+  size: number,
 }
