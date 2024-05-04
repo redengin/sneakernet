efbf1574223f8cfca2015a48ab3536f8efbd4e79
redengin <sholstei@yahoo.com>
2024-05-03 22:23:53 -0400

    essid code complete

--- firmware/rust/rp2040/src/main.rs
+++ firmware/rust/rp2040/src/main.rs
@@ -4,7 +4,6 @@
 
 // use core::fmt::Debug;
 
-use embassy_net::driver::Driver;
 // debugging support
 use panic_abort as _;   // abort upon panic (TODO change for testing)
 use defmt_rtt as _;     // use defmt for probe logging
@@ -16,18 +15,13 @@ use embassy_executor::Spawner;
 use static_cell::StaticCell;
 use heapless::Vec;
 
-// RP2040 support
-use embassy_rp::bind_interrupts;
+// embassy RP2040 support
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::pio::{InterruptHandler, Pio};
-// use embassy_rp::peripherals::{DMA_CH0, PIO0};
-use embassy_rp::peripherals::{PIO0};
-use cyw43::{State};
-use cyw43_pio::PioSpi;
 
 
-bind_interrupts!(struct Irqs {
-    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+embassy_rp::bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<embassy_rp::peripherals::PIO0>;
 });
 
 
@@ -47,9 +41,9 @@ async fn main(_spawner: Spawner) {
     let pwr = Output::new(p.PIN_23, Level::Low);
     let cs = Output::new(p.PIN_25, Level::High);
     let mut pio = Pio::new(p.PIO0, Irqs);
-    let spi = PioSpi::new(&mut pio.common, pio.sm0, pio.irq0, cs, p.PIN_24, p.PIN_29, p.DMA_CH0);
-    static STATE: StaticCell<State> = StaticCell::new();
-    let state = STATE.init(State::new());
+    let spi = cyw43_pio::PioSpi::new(&mut pio.common, pio.sm0, pio.irq0, cs, p.PIN_24, p.PIN_29, p.DMA_CH0);
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
     let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
     let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
     let (net_device, control, runner) = cyw43::new(state, pwr, spi, fw).await;
@@ -79,7 +73,7 @@ async fn main(_spawner: Spawner) {
     // TODO choose an optimal channel
     let channel = 9;
     control.start_ap_open(
-        sneakernet::essid(net_device.hardware_address()),
+        core::str::from_utf8(&sneakernet::essid(net_device)).unwrap(),
         channel)
         .await;
 
--- firmware/rust/sneakernet/src/lib.rs
+++ firmware/rust/sneakernet/src/lib.rs
@@ -1,12 +1,35 @@
 #![no_std]
 
 
-
 pub static IP_ADDRESS:embassy_net::Ipv4Cidr =
     embassy_net::Ipv4Cidr::new(embassy_net::Ipv4Address::new(192, 168, 4, 1), 24);
 
-pub static SSID_PREFIX:&str = "SneakerNet";
-pub fn essid(_mac:embassy_net_driver::HardwareAddress) -> &'static str {
+
+const SSID_PREFIX:&[u8] = "SneakerNet".as_bytes();
+pub fn essid<NetDevice:embassy_net_driver::Driver>(net_device:NetDevice) -> [u8; SSID_PREFIX.len() + 1 + (2*6)] {
     // FIXME return "SneakerNet 01020304050607" (where hex represents the hardware MAC)
-    return SSID_PREFIX;
+    let mut essid = [0u8; SSID_PREFIX.len() + 1 + (2*6)];
+    essid.copy_from_slice(SSID_PREFIX);
+    essid[SSID_PREFIX.len()] = b' ';
+
+    if let embassy_net_driver::HardwareAddress::Ethernet(mac) = net_device.hardware_address()
+    {
+        for i in 0..5
+        {
+            essid[SSID_PREFIX.len() + 1 + (2*i)] = hex(mac[i]/16);
+            essid[SSID_PREFIX.len() + 1 + (2*i)+1] = hex(mac[i]%16);
+        }
+    }
+    else { panic!("was not an ethernet device") };
+
+    // let hw_addr = net_device.hardware_address();
+
+
+    return essid;
+}
+
+fn hex(v:u8) -> u8
+{
+    if v < 10 { b'0' + v }
+    else { b'A' + (v - 10) }
 }
