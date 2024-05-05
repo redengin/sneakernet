b42fa18da2146cd25ddd6640cf67525972949e4c
redengin <sholstei@yahoo.com>
2024-05-04 22:34:08 -0400

    in work, returning to rust stable

--- firmware/rust/rp2040/src/main.rs
+++ firmware/rust/rp2040/src/main.rs
@@ -1,37 +1,34 @@
 #![no_std]
 #![no_main]
-// #![allow(async_fn_in_trait)]
 
-// use core::fmt::Debug;
-
-// debugging support
-use panic_abort as _;   // abort upon panic (TODO change for testing)
-use defmt_rtt as _;     // use defmt for probe logging
-// use core::str::from_utf8;
-use defmt::*;
-
-// Embassy support
-use embassy_executor::Spawner;
-use static_cell::StaticCell;
+use defmt_rtt as _;
+use panic_abort as _;
 use heapless::Vec;
 
-// embassy RP2040 support
+use cyw43_pio::PioSpi;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output};
+use embassy_rp::peripherals::{DMA_CH0, PIN_23, PIN_25, PIO0};
 use embassy_rp::pio::{InterruptHandler, Pio};
+// use embassy_time::{Duration, Timer};
+use static_cell::StaticCell;
 
-
-embassy_rp::bind_interrupts!(struct Irqs {
-    PIO0_IRQ_0 => InterruptHandler<embassy_rp::peripherals::PIO0>;
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
 });
 
 
-// #[embassy_executor::task]
-// async fn net_task(stack: &'static Stack<cyw43::NetDriver<'static>>) -> ! {
-//     stack.run().await
-// }
+#[embassy_executor::task]
+async fn wifi_task(runner: cyw43::Runner<'static, Output<'static, PIN_23>, PioSpi<'static, PIN_25, PIO0, 0, DMA_CH0>>)
+{
+    runner.run().await
+}
+
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     info!("SneakerNet starting....");
 
     // initialize RP2040 hardware
@@ -46,8 +43,8 @@ async fn main(_spawner: Spawner) {
     let state = STATE.init(cyw43::State::new());
     let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
     let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
-    let (net_device, control, runner) = cyw43::new(state, pwr, spi, fw).await;
-    runner.run().await;
+    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    (spawner.spawn(wifi_task(runner))).unwrap();
     control.init(clm).await;
     control.set_power_management(cyw43::PowerManagementMode::PowerSave).await;
 
@@ -67,14 +64,24 @@ async fn main(_spawner: Spawner) {
         RESOURCES.init(embassy_net::StackResources::<2>::new()),
         seed,
     ));
-    stack.run().await;
+    // stack.run().await;
 
-    // create an open WiFi access point
-    // TODO choose an optimal channel
-    let channel = 9;
-    control.start_ap_open(
-        core::str::from_utf8(&sneakernet::essid(net_device)).unwrap(),
-        channel)
-        .await;
+    // // create an open WiFi access point
+    // // TODO choose an optimal channel
+    // let channel = 9;
+    // control.start_ap_open(
+    //     core::str::from_utf8(&sneakernet::essid(net_device)).unwrap(),
+    //     channel)
+    //     .await;
 
 }
+
+    // // create an open WiFi access point
+    // // TODO choose an optimal channel
+    // let channel = 9;
+    // control.start_ap_open(
+    //     core::str::from_utf8(&sneakernet::essid(net_device)).unwrap(),
+    //     channel)
+    //     .await;
+
+
