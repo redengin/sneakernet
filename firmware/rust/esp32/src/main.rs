14347077b88fabfba3e91a23c909fb0d8f39bfd1
redengin <sholstei@yahoo.com>
2024-08-03 00:38:15 -0400

    in work

--- firmware/rust/esp32/src/main.rs
+++ firmware/rust/esp32/src/main.rs
@@ -1,140 +1,145 @@
 #![no_std]
 #![no_main]
-// handle exceptions and logging
 use esp_backtrace as _;
-use esp_println::println;
-// ESP hardware
-use esp_hal::{
-    prelude::*,
-    peripherals::Peripherals,
-    system::SystemControl,
-    clock::ClockControl,
-    rng::Rng,
-    timer::timg::TimerGroup,
-};
-use esp_wifi::{
-    wifi::{
-        WifiDevice,
-        WifiApDevice,
-        WifiController,
-        WifiState,
-        WifiEvent,
-        Configuration,
-        AccessPointConfiguration,
-    },
-    EspWifiInitFor,
-};
 
-use embassy_executor::Spawner;
-
-
-// FIXME move embassy_net usage to sneakernet
-use embassy_net::{
-    StaticConfigV4,
-    Stack,
-    StackResources,
-    Ipv4Cidr,
-    // IpListenEndpoint,
-    // tcp::TcpSocket,
-};
-use embassy_time::{Duration, Timer};
-
-macro_rules! mk_static {
-    ($t:ty,$val:expr) => {{
-        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
-        #[deny(unused_attributes)]
-        let x = STATIC_CELL.uninit().write(($val));
-        x
-    }};
+#[esp_hal_embassy::main]
+async fn main(_spawner: embassy_executor::Spawner) -> ! {
+    loop{}
 }
 
-#[main]
-async fn main(spawner: Spawner) -> ! {
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-    // freeze the clocks during initialization
-    let clocks = ClockControl::max(system.clock_control).freeze();
+// use esp_println::println;
+// // ESP hardware
+// use esp_hal::{
+//     prelude::*,
+//     peripherals::Peripherals,
+//     system::SystemControl,
+//     clock::ClockControl,
+//     rng::Rng,
+//     timer::timg::TimerGroup,
+// };
+// use esp_wifi::{
+//     wifi::{
+//         WifiDevice,
+//         WifiApDevice,
+//         WifiController,
+//         WifiState,
+//         WifiEvent,
+//         Configuration,
+//         AccessPointConfiguration,
+//     },
+//     EspWifiInitFor,
+// };
 
-    // initialize wifi
-    #[cfg(target_arch = "xtensa")]
-    let timer = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1, &clocks, None).timer0;
-    #[cfg(target_arch = "riscv32")]
-    let timer = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER).alarm0;
-    let wifi_init = esp_wifi::initialize(
-        EspWifiInitFor::Wifi,
-        timer,
-        Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-        &clocks,
-    ).unwrap();
-    let wifi = peripherals.WIFI;
-    let (wifi_interface, controller) = esp_wifi::wifi::new_with_mode(&wifi_init, wifi, WifiApDevice).unwrap();
+// use embassy_executor::Spawner;
 
-    // restart the clock
-    let timer_group0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
-    esp_hal_embassy::init(&clocks, timer_group0);
 
-    // create the network stack
-    let net_config = embassy_net::Config::ipv4_static(StaticConfigV4 {
-        address: Ipv4Cidr::new(sneakernet::IP_ADDRESS, 24),
-        gateway: None,
-        dns_servers: Default::default(),
-    });
-    const MAX_SOCKETS:usize = 20;
-    let stack = &*mk_static!(
-        Stack<WifiDevice<'_, WifiApDevice>>,
-        Stack::new(
-            wifi_interface,
-            net_config,
-            mk_static!(StackResources<MAX_SOCKETS>, StackResources::<MAX_SOCKETS>::new()),
-            Default::default(),
-        )
-    );
+// // FIXME move embassy_net usage to sneakernet
+// use embassy_net::{
+//     StaticConfigV4,
+//     Stack,
+//     StackResources,
+//     Ipv4Cidr,
+//     // IpListenEndpoint,
+//     // tcp::TcpSocket,
+// };
+// use embassy_time::{Duration, Timer};
 
-    // spawner.spawn(net_task(&stack)).ok();
-    spawner.spawn(wifi_ap(controller)).ok();
-
-    // await network stack initialization
-    loop {
-        if stack.is_link_up() {
-            break;
-        }
-        Timer::after(Duration::from_millis(500)).await;
-    }
-
-    loop {}
-}
-
-// #[embassy_executor::task]
-// async fn net_task(stack: &'static Stack<WifiDevice<'static, WifiApDevice>>) {
-//     stack.run().await
+// macro_rules! mk_static {
+//     ($t:ty,$val:expr) => {{
+//         static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+//         #[deny(unused_attributes)]
+//         let x = STATIC_CELL.uninit().write(($val));
+//         x
+//     }};
 // }
 
-#[embassy_executor::task]
-async fn wifi_ap(mut controller: WifiController<'static>) {
-    println!("start connection task");
-    println!("Device capabilities: {:?}", controller.get_capabilities());
-    loop {
-        match esp_wifi::wifi::get_wifi_state() {
-            WifiState::ApStarted => {
-                // wait until we're no longer connected
-                controller.wait_for_event(WifiEvent::ApStop).await;
-                Timer::after(Duration::from_millis(5000)).await
-            }
-            _ => {}
-        }
-        if !matches!(controller.is_started(), Ok(true)) {
-            let client_config = Configuration::AccessPoint(AccessPointConfiguration {
-                ssid: "esp-wifi".try_into().unwrap(),
-                ..Default::default()
-            });
-            controller.set_configuration(&client_config).unwrap();
-            println!("Starting wifi");
-            controller.start().await.unwrap();
-            println!("Wifi started!");
-        }
-    }
-}
+// #[main]
+// async fn main(spawner: Spawner) -> ! {
+//     let peripherals = Peripherals::take();
+//     let system = SystemControl::new(peripherals.SYSTEM);
+//     // freeze the clocks during initialization
+//     let clocks = ClockControl::max(system.clock_control).freeze();
+
+//     // initialize wifi
+//     #[cfg(target_arch = "xtensa")]
+//     let timer = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1, &clocks, None).timer0;
+//     #[cfg(target_arch = "riscv32")]
+//     let timer = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER).alarm0;
+//     let wifi_init = esp_wifi::initialize(
+//         EspWifiInitFor::Wifi,
+//         timer,
+//         Rng::new(peripherals.RNG),
+//         peripherals.RADIO_CLK,
+//         &clocks,
+//     ).unwrap();
+//     let wifi = peripherals.WIFI;
+//     let (wifi_interface, controller) = esp_wifi::wifi::new_with_mode(&wifi_init, wifi, WifiApDevice).unwrap();
+
+//     // restart the clock
+//     let timer_group0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
+//     esp_hal_embassy::init(&clocks, timer_group0);
+
+//     // create the network stack
+//     let net_config = embassy_net::Config::ipv4_static(StaticConfigV4 {
+//         address: Ipv4Cidr::new(sneakernet::IP_ADDRESS, 24),
+//         gateway: None,
+//         dns_servers: Default::default(),
+//     });
+//     const MAX_SOCKETS:usize = 20;
+//     let stack = &*mk_static!(
+//         Stack<WifiDevice<'_, WifiApDevice>>,
+//         Stack::new(
+//             wifi_interface,
+//             net_config,
+//             mk_static!(StackResources<MAX_SOCKETS>, StackResources::<MAX_SOCKETS>::new()),
+//             Default::default(),
+//         )
+//     );
+
+//     // spawner.spawn(net_task(&stack)).ok();
+//     spawner.spawn(wifi_ap(controller)).ok();
+
+//     // await network stack initialization
+//     loop {
+//         if stack.is_link_up() {
+//             break;
+//         }
+//         Timer::after(Duration::from_millis(500)).await;
+//     }
+
+//     loop {}
+// }
+
+// // #[embassy_executor::task]
+// // async fn net_task(stack: &'static Stack<WifiDevice<'static, WifiApDevice>>) {
+// //     stack.run().await
+// // }
+
+// #[embassy_executor::task]
+// async fn wifi_ap(mut controller: WifiController<'static>) {
+//     println!("start connection task");
+//     println!("Device capabilities: {:?}", controller.get_capabilities());
+//     loop {
+//         match esp_wifi::wifi::get_wifi_state() {
+//             WifiState::ApStarted => {
+//                 // wait until we're no longer connected
+//                 controller.wait_for_event(WifiEvent::ApStop).await;
+//                 Timer::after(Duration::from_millis(5000)).await
+//             }
+//             _ => {}
+//         }
+//         if !matches!(controller.is_started(), Ok(true)) {
+//             let client_config = Configuration::AccessPoint(AccessPointConfiguration {
+//                 ssid: "esp-wifi".try_into().unwrap(),
+//                 ..Default::default()
+//             });
+//             controller.set_configuration(&client_config).unwrap();
+//             println!("Starting wifi");
+//             controller.start().await.unwrap();
+//             println!("Wifi started!");
+//         }
+//     }
+// }
 
 //     esp_println::logger::init_logger_from_env();
 
