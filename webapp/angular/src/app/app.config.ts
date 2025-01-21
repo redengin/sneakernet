564c45389511013a5d1fada5bf5279c3be843548
redengin <sholstei@yahoo.com>
2025-01-21 00:02:24 -0500

    adding material

--- webapp/angular/src/app/app.config.ts
+++ webapp/angular/src/app/app.config.ts
@@ -3,6 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// FIXME what is this?
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
