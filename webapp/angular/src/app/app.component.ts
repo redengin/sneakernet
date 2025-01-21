b49d745df0823495b3cfe431d446e04766949ed5
redengin <sholstei@yahoo.com>
2025-01-21 00:27:17 -0500

    in work

--- webapp/angular/src/app/app.component.ts
+++ webapp/angular/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.html',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+})
+export class AppToolbar{
+  title = 'Sneakernet';
+};
+
+
+
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    RouterOutlet,
+    AppToolbar,
+  ],
 })
 export class AppComponent {
-  title = 'sneakernet';
 }
