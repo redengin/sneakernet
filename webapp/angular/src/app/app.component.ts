e3765f2ad84c03a93b42f303f08f9b940f285c15
redengin <sholstei@yahoo.com>
2025-01-29 00:27:43 -0500

    in work

--- webapp/angular/src/app/app.component.ts
+++ webapp/angular/src/app/app.component.ts
@@ -1,21 +1,59 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIcon } from '@ng-icons/core';
+import { inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MatDialogTitle, MatDialogContent } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+export var title = "Sneakernet"
 
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.html',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, NgIcon],
 })
 export class AppToolbar{
-  title = 'Sneakernet';
+  readonly title = title;
+
+  readonly dialog = inject(MatDialog);
+  openDialog(): void {
+    this.dialog.open(AboutDialog);
+  }
+};
+@Component({
+  selector: 'about-dialog',
+  templateUrl: './about_dialog.html',
+  imports: [MatDialogTitle, MatDialogContent],
+})
+export class AboutDialog{
+  readonly title = title;
+  readonly dialogRef = inject(MatDialogRef<AboutDialog>);
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 };
 
 
 
-import { RouterOutlet } from '@angular/router';
+type PathData = {
+  locked?: boolean;
+  subfolders?: string[];
+  files?: Content[];
+};
+type Content = {
+  name?: string;
+  size?: BigInteger;
+  timestamp?: string;
+  title?: string;
+  hasIcon?: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -23,7 +61,21 @@ import { RouterOutlet } from '@angular/router';
   imports: [
     RouterOutlet,
     AppToolbar,
+    MatInputModule, MatSelectModule, MatFormFieldModule,
+    MatButtonModule, NgIcon,
   ],
 })
 export class AppComponent {
+
+  currentPath = "/";
+
+  pathData : PathData = {
+    // locked: true,
+    // subfolders: ['hello', 'world'],
+  };
+
+  createSubfolder() : void
+  {
+
+  }
 }
--- webapp/angular/src/app/app.config.ts
+++ webapp/angular/src/app/app.config.ts
@@ -3,9 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-// FIXME what is this?
+// FIXME do we want material animations?
 // import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+// TODO use @ng-icons to embed material icons
+import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
+import { matInfo, matCreateNewFolder } from '@ng-icons/material-icons/baseline';
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideIcons({ matInfo, matCreateNewFolder }),
+  ]
 };
