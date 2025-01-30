f9041d46a6a9a44db25d55c87a83ad7878313c26
redengin <sholstei@yahoo.com>
2025-01-29 22:49:40 -0500

    newfolder dialog in work

--- webapp/angular/src/app/app.component.ts
+++ webapp/angular/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { NgIcon } from '@ng-icons/core';
-import { inject } from '@angular/core';
-import { MatDialog, MatDialogRef, MatDialogTitle, MatDialogContent } from '@angular/material/dialog';
+import { inject, signal, model } from '@angular/core';
+import { MatDialog, MatDialogRef, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 
 export var title = "Sneakernet"
 
@@ -18,7 +19,7 @@ export var title = "Sneakernet"
   templateUrl: './toolbar.html',
   imports: [MatToolbarModule, MatButtonModule, NgIcon],
 })
-export class AppToolbar{
+export class AppToolbar {
   readonly title = title;
 
   readonly dialog = inject(MatDialog);
@@ -27,11 +28,10 @@ export class AppToolbar{
   }
 };
 @Component({
-  selector: 'about-dialog',
   templateUrl: './about_dialog.html',
   imports: [MatDialogTitle, MatDialogContent],
 })
-export class AboutDialog{
+export class AboutDialog {
   readonly title = title;
   readonly dialogRef = inject(MatDialogRef<AboutDialog>);
   onNoClick(): void {
@@ -69,13 +69,49 @@ export class AppComponent {
 
   currentPath = "/";
 
-  pathData : PathData = {
+  pathData: PathData = {
     // locked: true,
     // subfolders: ['hello', 'world'],
   };
 
-  createSubfolder() : void
-  {
+  readonly dialog = inject(MatDialog)
+// New Folder handler
+  // readonly newfolder = signal('');
+  readonly newfolder = signal('');
+  openNewFolderDialog(): void {
+    const dialogRef = this.dialog.open(NewFolderDialog, {
+      // data: {name: this.name(), animal: this.animal()},
+      data: {
+        currentPath: this.currentPath,
+        newfolder: this.newfolder(),
+      }
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`The dialog was closed result:${result}`);
+      // if (result !== undefined) {
+      //   this.newfolder.set(result);
+      // }
+    });
   }
+
 }
+export interface NewFolderDialogData {
+  currentPath: string;
+  newfolder: string;
+}
+@Component({
+  templateUrl: './new_folder_dialog.html',
+  imports: [
+    MatDialogTitle, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose,
+    FormsModule, MatFormField, MatLabel, MatInputModule,
+  ],
+})
+export class NewFolderDialog {
+  readonly dialogRef = inject(MatDialogRef<AboutDialog>);
+  readonly data = inject<NewFolderDialogData>(MAT_DIALOG_DATA);
+  readonly newfolder = model(this.data.newfolder);
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+};
--- webapp/angular/src/app/app.config.ts
+++ webapp/angular/src/app/app.config.ts
@@ -3,10 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-// FIXME do we want material animations?
-// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-// TODO use @ng-icons to embed material icons
+// use @ng-icons to embed material icons
 import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
 import { matInfo, matCreateNewFolder } from '@ng-icons/material-icons/baseline';
 
