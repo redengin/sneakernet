8bc7dcd5c0b5c76c2795429cdf9314f5bd4e1058
redengin <sholstei@yahoo.com>
2025-02-06 23:09:07 -0500

    in work

--- webapp/angular/src/app/app.config.ts
+++ webapp/angular/src/app/app.config.ts
@@ -1,31 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideNoopAnimations } from '@angular/platform-browser/animations';
-
-import { provideHttpClient } from '@angular/common/http';
-
 import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
 
-// use @ng-icons to embed material icons
-import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
-import { matInfo, matCreateNewFolder } from '@ng-icons/material-icons/baseline';
-import { matFolderOutline, matFileDownloadOutline, matDeleteOutline } from '@ng-icons/material-icons/outline';
-
-
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideNoopAnimations(), // disable animation
-    provideHttpClient(),
-
-    provideIcons({
-      matInfo, matCreateNewFolder,
-      matFolderOutline, matFileDownloadOutline, matDeleteOutline
-    }),
-
-    // provideZoneChangeDetection({ eventCoalescing: true }),
-    // provideRouter(routes),
-    // TODO provide input validation
-    // { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher }
-    // provideHttpClientTesting(),
-   ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
