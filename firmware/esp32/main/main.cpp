564c37e8aeabe7a9ffb0afbcd59938e6fc87224b
redengin <sholstei@yahoo.com>
2025-02-14 00:06:57 -0500

    cleaned up

--- firmware/esp32/main/main.cpp
+++ firmware/esp32/main/main.cpp
@@ -15,6 +15,13 @@ void app_main(void)
     // use the configured logging level for project
     esp_log_level_set("*", static_cast<esp_log_level_t>(CONFIG_SNEAKERNET_LOG_LEVEL));
 
+    // provide the storage
+    static SdCard sdcard;
+
+    // provide the catalog
+    static Catalog catalog(sdcard.root());
+
+    // keep track of used sockets, so that WebServer can use the remaining
     size_t available_sockets_count = CONFIG_LWIP_MAX_SOCKETS;
 
     // create the access point
@@ -24,12 +31,6 @@ void app_main(void)
 
     // provide the frontend
     static WebServer webserver(available_sockets_count);
-
-    // provide the storage
-    static SdCard sdcard;
-
-    // provide the catalog
-    static Catalog catalog(sdcard.root());
     // register the catalog REST API
     rest::catalog::registerHandlers(webserver, catalog);
 
