8d6bb6be6b249d76edea21d4820ca98d9183df72
redengin <sholstei@yahoo.com>
2025-02-19 00:15:40 -0500

    in work

--- firmware/test/conftest.py
+++ firmware/test/conftest.py
@@ -1,8 +1,20 @@
 import pytest
 
-from pywifi import PyWiFi, const, Profile
+import pywifi
 
 @pytest.fixture
 def sneakernet():
-    pass
+    wifi = pywifi.PyWiFi()
 
+    # FIXME requires root access
+    # iface = wifi.interfaces()[0]
+
+    # connect to SneakerNet node
+    profile = pywifi.Profile()
+    profile.ssid = "SneakerNet"
+    profile.auth = pywifi.const.AUTH_ALG_OPEN
+
+    yield
+
+    # FIXME requires root access
+    # iface.disconnect()
