285fdc7af3eedd8cfe88686a81c5c49b09027686
redengin <sholstei@yahoo.com>
2025-02-20 13:24:17 -0500

    in work

--- firmware/test/conftest.py
+++ firmware/test/conftest.py
@@ -6,13 +6,18 @@ import pywifi
 def sneakernet():
     wifi = pywifi.PyWiFi()
 
-    # FIXME requires root access
-    # iface = wifi.interfaces()[0]
+    # requires netdev group access
+    try:
+        iface = wifi.interfaces()[0]
+    except PermissionError as e:
+        print(e)
+        pytest.fail("make sure user is part of 'netdev' group")
 
     # connect to SneakerNet node
     profile = pywifi.Profile()
     profile.ssid = "SneakerNet"
     profile.auth = pywifi.const.AUTH_ALG_OPEN
+    profile.key = pywifi.const.AKM_TYPE_NONE
 
     yield
 
