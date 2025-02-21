778ea2dece23e32c76a453921eb6761af12669c2
redengin <sholstei@yahoo.com>
2025-02-21 00:55:52 -0500

    in work

--- firmware/pytest/test_catalog.py
+++ firmware/pytest/test_catalog.py
@@ -1,40 +1,81 @@
 import pytest
 import requests
 
-
 catalog_base_url = "http://192.168.4.1/api/catalog/"
 
 
 def test_catalog(sneakernet):
-    TEST_FOLDER = "test/"
-    FILE_NAME = "testfile"
-    FILE_SIZE = 100000
-    FILE_PAYLOAD = b"X" * FILE_SIZE
-    TIMESTAMP = "2025-01-06T01:02:03Z"
+    # test root file creation/deletion
+    # upload a file
+    TEST_FILENAME = "testing..."
+    assert 200 == createFile(TEST_FILENAME)
 
-    # create a new file
-    test_url = catalog_base_url + TEST_FOLDER + FILE_NAME
-    response = requests.put(test_url,
-                            headers={
-                                "X-FileTimeStamp":TIMESTAMP,
-                            },
-                            data=FILE_PAYLOAD)
-    assert 200 == response.status_code
-
-    # retrieve that file
-    response = requests.get(test_url)
-    assert 200 == response.status_code
-    assert FILE_SIZE == len(response.content)
-    assert FILE_PAYLOAD == response.content
-    print(f"expected {TIMESTAMP} got {response.headers['X-FileTimeStamp']}")
-    # FIXME off by one second (so only comparing upto the minute)
-    assert TIMESTAMP[0:17] == response.headers["X-FileTimeStamp"][0:17]
+    # TODO validate file
 
     # remove the file
-    response = requests.delete(test_url)
-    assert 200 == response.status_code
+    assert 200 == deleteFile(TEST_FILENAME)
 
-    # remove the test directory
-    test_url = catalog_base_url + TEST_FOLDER
-    response = requests.delete(test_url)
-    assert 200 == response.status_code
+# test directory file creation/deletion
+    # create a directory
+    TEST_DIRECTORY = "test/"
+    assert 200 == requests.put(catalog_base_url + TEST_DIRECTORY).status_code
+
+    # upload a file into directory
+    assert 200 == createFile(TEST_DIRECTORY + TEST_FILENAME)
+
+    # TODO validate file
+
+    # remove the file
+    assert 200 == deleteFile(TEST_DIRECTORY + TEST_FILENAME)
+
+    # remove a directory
+    TEST_DIRECTORY = "test/"
+    assert 200 == requests.delete(
+        catalog_base_url + TEST_DIRECTORY).status_code
+
+# test changing file title
+    # upload a file
+    assert 200 == createFile(TEST_FILENAME)
+
+    # change title
+    assert 200 == requests.put(catalog_base_url + TEST_FILENAME,
+                               params={
+                                   "title": "this is a test"
+                               }).status_code
+
+    # TODO validate title change
+
+    # remove the file
+    assert 200 == deleteFile(TEST_FILENAME)
+
+# test changing file icon
+    # upload a file
+    assert 200 == createFile(TEST_FILENAME)
+
+    # change icon
+    ICON_DATA = b'X' * 100
+    assert 200 == requests.put(catalog_base_url + TEST_FILENAME + "?icon",
+                               data=ICON_DATA).status_code
+
+    # TODO validate icon change
+
+    # get icon
+    assert 200 == requests.get(catalog_base_url + TEST_FILENAME + "?icon").status_code
+
+    # remove the file
+    assert 200 == deleteFile(TEST_FILENAME)
+
+
+# test utilities
+# ===============================================================================
+def createFile(filepath: str):
+    return requests.put(catalog_base_url + filepath,
+                        headers={
+                            "X-timestamp": "2025-01-06T01:02:03Z"
+                        },
+                        data="0"
+                        ).status_code
+
+
+def deleteFile(filepath: str):
+    return requests.delete(catalog_base_url + filepath).status_code
