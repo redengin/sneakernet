3606d7bfb27bb9ee6839075dbd712c43aebb5cfd
redengin <sholstei@yahoo.com>
2025-02-20 21:05:40 -0500

    working wifi fixture

--- firmware/test/conftest.py
+++ firmware/test/conftest.py
@@ -2,24 +2,50 @@ import pytest
 
 import pywifi
 
+import time
+
 @pytest.fixture
 def sneakernet():
-    wifi = pywifi.PyWiFi()
 
-    # requires netdev group access
-    try:
-        iface = wifi.interfaces()[0]
-    except PermissionError as e:
-        print(e)
-        pytest.fail("make sure user is part of 'netdev' group")
-
-    # connect to SneakerNet node
+    # SneakerNet node profile
     profile = pywifi.Profile()
     profile.ssid = "SneakerNet"
     profile.auth = pywifi.const.AUTH_ALG_OPEN
     profile.key = pywifi.const.AKM_TYPE_NONE
 
+    wifi = pywifi.PyWiFi()
+    iface = None
+    try:
+        iface = wifi.interfaces()[1]
+    except PermissionError as e:
+        print(e)
+        pytest.fail("make sure user is part of 'netdev' group")
+
+    # connect to sneakernet
+    p = iface.add_network_profile(profile) 
+    iface.connect(p)
+    time.sleep(1)
+    while True:
+        status = iface.status()
+        if status in [pywifi.const.IFACE_CONNECTING, pywifi.const.IFACE_SCANNING]:
+            continue
+        break
+    assert iface.status() == pywifi.const.IFACE_CONNECTED
+
+    # perform the test
     yield
 
-    # FIXME requires root access
-    # iface.disconnect()
+    # disconnect from sneakernet
+    iface.disconnect()
+
+
+if __name__ == "__main__":
+    ''' provide info about pywifi
+    '''
+    wifi = pywifi.PyWiFi()
+    iface = wifi.interfaces()[1]
+    print(f"Using wifi: {iface.name()}")
+
+    print("Profiles:")
+    for p in iface.network_profiles():
+        print(f"\t{vars(p)}")
