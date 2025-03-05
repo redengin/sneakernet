aff6fc59fb29ce8f5c611c34202e31f8505174d3
redengin <sholstei@yahoo.com>
2025-03-05 00:25:33 -0500

    new project using esp-generate

---
+++ firmware/rust/esp32/README.md
@@ -0,0 +1,12 @@
+Install RUST ESP32 support
+================================================================================
+* [RISC-V and Xtenssa](https://docs.esp-rs.org/book/installation/riscv-and-xtensa.html)
+* [RISC-V only](https://docs.esp-rs.org/book/installation/riscv.html)
+
+Install ESP-FLASH (used to flash and monitor hardware)
+================================================================================
+https://docs.esp-rs.org/book/tooling/espflash.html
+
+```sh
+cargo install cargo-espflash espflash
+```
---
+++ firmware/rust/esp32/src/bin/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
+use esp_hal::time::{Duration, Instant};
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[main]
+fn main() -> ! {
+    // generator version: 0.3.1
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let _peripherals = esp_hal::init(config);
+
+    loop {
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
+}
