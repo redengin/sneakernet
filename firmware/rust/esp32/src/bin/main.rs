eaf7381947c7e99bcf9a8b49992c0bc1c66827c7
redengin <sholstei@yahoo.com>
2025-03-06 23:23:13 -0500

    in work

--- firmware/rust/esp32/src/bin/main.rs
+++ firmware/rust/esp32/src/bin/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+// panic handler
+use esp_backtrace as _;
 
 // use esp_hal::clock::CpuClock;
 // use esp_hal::main;
@@ -25,33 +27,32 @@
 //     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
 // }
 
-use esp_backtrace as _;
-use embassy_executor::Spawner;
+// use embassy_executor::Spawner;
 // use esp_hal::timer::timg::TimerGroup;
 // use embassy_time::{Duration, Timer};
 
-#[embassy_executor::task]
-async fn run() {
-    loop {
-        esp_println::println!("Hello world from embassy using esp-hal-async!");
-        Timer::after(Duration::from_millis(1_000)).await;
-    }
-}
+// #[embassy_executor::task]
+// async fn run() {
+//     loop {
+//         esp_println::println!("Hello world from embassy using esp-hal-async!");
+//         Timer::after(Duration::from_millis(1_000)).await;
+//     }
+// }
 
 #[esp_hal_embassy::main]
-async fn main(spawner: Spawner) {
+async fn main(spawner: embassy_executor::Spawner) {
     esp_println::logger::init_logger_from_env();
     let peripherals = esp_hal::init(esp_hal::Config::default());
 
     esp_println::println!("Init!");
 
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    esp_hal_embassy::init(timg0.timer0);
+    // let timg0 = TimerGroup::new(peripherals.TIMG0);
+    // esp_hal_embassy::init(timg0.timer0);
 
-    spawner.spawn(run()).ok();
+    // spawner.spawn(run()).ok();
 
-    loop {
-        esp_println::println!("Bing!");
-        Timer::after(Duration::from_millis(5_000)).await;
-    }
+    // loop {
+    //     esp_println::println!("Bing!");
+    //     Timer::after(Duration::from_millis(5_000)).await;
+    // }
 }
