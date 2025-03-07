14c5d01bf5b529d1cbfbf924deccee4ef3d485dc
redengin <sholstei@yahoo.com>
2025-03-06 20:35:09 -0500

    in work

--- firmware/esp32/main/WebServer.cpp
+++ firmware/esp32/main/WebServer.cpp
@@ -32,20 +32,18 @@ WebServer::WebServer(const size_t max_sockets) {
     // allow wildcard uris
     httpsConfig.httpd.uri_match_fn = httpd_uri_match_wildcard;
     // provide the private key
-#if 0  // FIXME https doesn't understand keys
-        extern const unsigned char prvtkey_pem_start[] asm("_binary_sneakernet_pem_start");
-        extern const unsigned char prvtkey_pem_end[]   asm("_binary_sneakernet_pem_end");
-        httpsConfig.prvtkey_pem = prvtkey_pem_start;
-        httpsConfig.prvtkey_len = prvtkey_pem_end - prvtkey_pem_start;
-        // provide the public key
-        extern const unsigned char servercert_start[] asm("_binary_sneakernet_public_pem_start");
-        extern const unsigned char servercert_end[]   asm("_binary_sneakernet_public_pem_end");
-        httpsConfig.servercert = servercert_start;
-        httpsConfig.servercert_len = servercert_end - servercert_start;
-        ESP_ERROR_CHECK(httpd_ssl_start(&httpsHandle, &httpsConfig));
-        // upon 404, redirect to index
-        ESP_ERROR_CHECK(httpd_register_err_handler(httpsHandle, HTTPD_404_NOT_FOUND, redirect));
-#endif
+    extern const unsigned char prvtkey_pem_start[] asm("_binary_sneakernet_https_priv_pem_start");
+    extern const unsigned char prvtkey_pem_end[]   asm("_binary_sneakernet_https_priv_pem_end");
+    httpsConfig.prvtkey_pem = prvtkey_pem_start;
+    httpsConfig.prvtkey_len = prvtkey_pem_end - prvtkey_pem_start;
+    // provide the public key
+    extern const unsigned char servercert_start[] asm("_binary_sneakernet_https_pub_pem_start");
+    extern const unsigned char servercert_end[]   asm("_binary_sneakernet_https_pub_pem_end");
+    httpsConfig.servercert = servercert_start;
+    httpsConfig.servercert_len = servercert_end - servercert_start;
+    ESP_ERROR_CHECK(httpd_ssl_start(&httpsHandle, &httpsConfig));
+    // upon 404, redirect to index
+    ESP_ERROR_CHECK(httpd_register_err_handler(httpsHandle, HTTPD_404_NOT_FOUND, http_redirect));
   }
 
   // create HTTP server
