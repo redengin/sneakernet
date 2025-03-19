bfbdd7d020037551fe47fef6e6dbe84c1ef6a0fc
redengin <sholstei@yahoo.com>
2025-03-19 00:51:11 -0400

    in work

--- firmware/rust/esp32/src/bin/main.rs
+++ firmware/rust/esp32/src/bin/main.rs
@@ -3,56 +3,51 @@
 // panic handler
 use esp_backtrace as _;
 
-// use esp_hal::clock::CpuClock;
-// use esp_hal::main;
-// use esp_hal::time::{Duration, Instant};
+use embassy_executor::Spawner;
 
-// #[panic_handler]
-// fn panic(_: &core::panic::PanicInfo) -> ! {
-//     loop {}
-// }
 
-// #[main]
-// fn main() -> ! {
-//     // generator version: 0.3.1
-
-//     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-//     let _peripherals = esp_hal::init(config);
-
-//     loop {
-//         let delay_start = Instant::now();
-//         while delay_start.elapsed() < Duration::from_millis(500) {}
-//     }
-
-//     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
-// }
-
-// use embassy_executor::Spawner;
-// use esp_hal::timer::timg::TimerGroup;
-// use embassy_time::{Duration, Timer};
-
-// #[embassy_executor::task]
-// async fn run() {
-//     loop {
-//         esp_println::println!("Hello world from embassy using esp-hal-async!");
-//         Timer::after(Duration::from_millis(1_000)).await;
-//     }
-// }
+// When you are okay with using a nightly compiler it's better to use https://docs.rs/static_cell/2.1.0/static_cell/macro.make_static.html
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
+}
 
 #[esp_hal_embassy::main]
-async fn main(spawner: embassy_executor::Spawner) {
+async fn main(_spawner: Spawner) {
+    // initialize logger
     esp_println::logger::init_logger_from_env();
-    let peripherals = esp_hal::init(esp_hal::Config::default());
 
-    esp_println::println!("Init!");
+    // initialize the chip (max cpuclock required for wifi)
+    let peripherals = esp_hal::init(
+        esp_hal::Config::default().with_cpu_clock(esp_hal::clock::CpuClock::max())
+    );
 
-    // let timg0 = TimerGroup::new(peripherals.TIMG0);
-    // esp_hal_embassy::init(timg0.timer0);
+    // initialize embassy scheduler
+#[cfg(feature = "esp32")] {
+    let timg1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1);
+    esp_hal_embassy::init(timg1.timer0);
+}
+#[cfg(not(feature = "esp32"))] {
+    use esp_hal::timer::systimer::SystemTimer;
+    let systimer = SystemTimer::new(peripherals.SYSTIMER);
+    esp_hal_embassy::init(systimer.alarm0);
+}
 
-    // spawner.spawn(run()).ok();
+    // initialize wifi hardware
+    let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
+    let rng = esp_hal::rng::Rng::new(peripherals.RNG);
+    let wifi_init = &*mk_static!(
+        esp_wifi::EspWifiController<'static>,
+        esp_wifi::init(timg0.timer0, rng.clone(), peripherals.RADIO_CLK).unwrap()
+    );
+    let (wifi_interface, mut wifi_controller) =
+        esp_wifi::wifi::new_with_mode(&wifi_init, peripherals.WIFI, esp_wifi::wifi::WifiApDevice).unwrap();
 
-    // loop {
-    //     esp_println::println!("Bing!");
-    //     Timer::after(Duration::from_millis(5_000)).await;
-    // }
+
+
+
 }
