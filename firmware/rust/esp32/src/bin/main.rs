3e39f041770f11dd3da2115b0834076923ee6109
redengin <sholstei@yahoo.com>
2025-03-24 00:03:33 -0400

    in work

--- firmware/rust/esp32/src/bin/main.rs
+++ firmware/rust/esp32/src/bin/main.rs
@@ -2,8 +2,10 @@
 #![no_main]
 // panic handler
 use esp_backtrace as _;
+use esp_alloc as _;
 
-use sneakernet::{static_cell, make_static};
+use sneakernet::{static_cell, make_static, embassy_net};
+use sneakernet::embassy_net::StackResources;
 
 
 #[esp_hal_embassy::main]
@@ -15,6 +17,10 @@ async fn main(spawner: embassy_executor::Spawner) {
     let peripherals = esp_hal::init(
         esp_hal::Config::default().with_cpu_clock(esp_hal::clock::CpuClock::max())
     );
+
+    // initialize the heap
+    esp_alloc::heap_allocator!(72 * 1024);
+
 
     // initialize embassy scheduler
 #[cfg(feature = "esp32")] {
@@ -29,15 +35,46 @@ async fn main(spawner: embassy_executor::Spawner) {
 
     // initialize wifi hardware
     let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
-    let rng = esp_hal::rng::Rng::new(peripherals.RNG);
+    let mut rng = esp_hal::rng::Rng::new(peripherals.RNG);
     let wifi_init = &*make_static!(
         esp_wifi::EspWifiController<'static>,
         esp_wifi::init(timg0.timer0, rng.clone(), peripherals.RADIO_CLK).unwrap()
     );
-    let (_wifi_interface, mut _wifi_controller) =
+    let (wifi_device, mut wifi_controller) =
         esp_wifi::wifi::new_with_mode(&wifi_init, peripherals.WIFI, esp_wifi::wifi::WifiApDevice).unwrap();
 
-    // sneakernet::start(spawner, wifi_interface);
+    // configure the AccessPoint
+    let wifi_config = esp_wifi::wifi::Configuration::AccessPoint(
+        esp_wifi::wifi::AccessPointConfiguration{
+            ssid: sneakernet::ssid(wifi_device.mac_address()),
+            ..Default::default()
+        }
+    );
+    wifi_controller.set_configuration(&wifi_config).unwrap();
+
+    // start the network stack
+    let net_config = embassy_net::Config::ipv4_static(embassy_net::StaticConfigV4 {
+        address: embassy_net::Ipv4Cidr::new(sneakernet::IP_ADDRESS, 24),
+        gateway: None,
+        dns_servers: Default::default(),
+    });
+    let seed = (rng.random() as u64) << 32 | rng.random() as u64;
+    const SOCKETS_MAX:usize = 20; // TODO tune to optimize client support
+    let (_stack, _runner) = embassy_net::new(
+        wifi_device,
+        net_config,
+        make_static!(StackResources<{SOCKETS_MAX}>, StackResources::<{SOCKETS_MAX}>::new()),
+        seed,
+    );
+
+
+    // sneakernet::start(spawner, wifi_device);
     sneakernet::start(spawner);
+
+
+    // start the wifi AP
+    wifi_controller.start().unwrap();
+
+    loop{};
 
 }
