a33471c515a70ed337e9c5fc58baa1ab2e01cfab
redengin <sholstei@yahoo.com>
2025-03-27 22:30:02 -0400

    working

--- firmware/rust/esp32/src/bin/main.rs
+++ firmware/rust/esp32/src/bin/main.rs
@@ -5,8 +5,6 @@ use esp_alloc as _;
 
 use sneakernet::log;
 use sneakernet::{static_cell, make_static, embassy_net};
-use sneakernet::embassy_net::StackResources;
-use sneakernet::embassy_time;
 
 
 #[esp_hal_embassy::main]
@@ -14,7 +12,7 @@ async fn main(spawner: embassy_executor::Spawner) {
     // initialize logger
     esp_println::logger::init_logger_from_env();
 
-    // initialize the chip (max cpuclock required for wifi)
+    // initialize the chip (max cpuclock, required for wifi)
     let peripherals = esp_hal::init(
         esp_hal::Config::default()
             .with_cpu_clock(esp_hal::clock::CpuClock::max())
@@ -51,21 +49,20 @@ async fn main(spawner: embassy_executor::Spawner) {
     );
     wifi_controller.set_configuration(&wifi_config).unwrap();
 
-    // start the network stack
+    // create the network stack
     let net_config = embassy_net::Config::ipv4_static(embassy_net::StaticConfigV4 {
         address: embassy_net::Ipv4Cidr::new(sneakernet::IP_ADDRESS, 24),
-        gateway: None,
+        gateway: Some(sneakernet::IP_ADDRESS),
         dns_servers: Default::default(),
     });
     let seed = (rng.random() as u64) << 32 | rng.random() as u64;
     const SOCKETS_MAX:usize = 20; // TODO tune to optimize client support
-    let (net_stack, runner) = embassy_net::new(
+    let (net_stack, mut runner) = embassy_net::new(
         wifi_device,
         net_config,
-        make_static!(StackResources<{SOCKETS_MAX}>, StackResources::<{SOCKETS_MAX}>::new()),
+        make_static!(embassy_net::StackResources<{SOCKETS_MAX}>, embassy_net::StackResources::<{SOCKETS_MAX}>::new()),
         seed,
     );
-    spawner.spawn(net_task(runner)).unwrap();
 
     // start sneakernet
     sneakernet::start(spawner, net_stack);
@@ -75,21 +72,6 @@ async fn main(spawner: embassy_executor::Spawner) {
     wifi_controller.start().unwrap();
     log::info!("Publishing wifi ssid [{ssid}]");
 
-    // wait for network stack
-    loop {
-        if net_stack.is_link_up() {
-            log::debug!("network stack up");
-            break;
-        }
-        log::debug!("waiting for network stack up");
-        embassy_time::Timer::after(embassy_time::Duration::from_millis(500)).await;
-    }
-
-    loop{};
-
-}
-
-#[embassy_executor::task]
-async fn net_task(mut runner: embassy_net::Runner<'static, esp_wifi::wifi::WifiDevice<'static, esp_wifi::wifi::WifiApDevice>>) {
+    // enter the network stack loop
     runner.run().await
 }
