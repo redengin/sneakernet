a203e30edff23d66545e6ab0cb7e3cb254de650a
redengin <sholstei@yahoo.com>
2025-04-11 00:55:28 -0400

    WiFi channel limits

--- firmware/esp32/main/WifiAccessPoint.hpp
+++ firmware/esp32/main/WifiAccessPoint.hpp
@@ -6,14 +6,11 @@ class WifiAccessPoint {
 public:
     static constexpr char TAG[] = "WifiAccessPoint";    ///< ESP logging tag
 
+    // provide system context with number of sockets created
+    static constexpr int socketsUsed = 1;
+
     /// @brief Creates the Open Wifi Access Point
     /// @post call publish()
     /// @param ssid 
     WifiAccessPoint();
-
-    // provide system context with number of sockets created
-    static constexpr int socketsUsed = 1;
-
-    /// @brief change the published SSID
-    void setSsid(const std::string ssid);
 };
