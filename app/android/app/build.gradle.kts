53547e54d59b3a07eed510bc4d6d074c576a9f3b
redengin <sholstei@yahoo.com>
2025-06-26 22:22:30 -0400

    adding flutter baseline

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -1,3 +1,11 @@
+import java.util.Properties
+import java.io.FileInputStream
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -30,11 +38,20 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias        = keystoreProperties["keyAlias"] as String
+            keyPassword     = keystoreProperties["keyPassword"] as String
+            storeFile       = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword   = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
